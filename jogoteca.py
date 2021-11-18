412345e2d25ceada6e4f470bee6c11f022046a01
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2021-11-17 21:49:39 -0300

    Implementa a rota de inclusão de novo jogo com o método POST

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,18 +8,27 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
-    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
-    jogo3 = Jogo('Tetris', 'Desafio', 'PC')
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+jogo3 = Jogo('Tetris', 'Desafio', 'PC')
+lista = [jogo1, jogo2, jogo3]
 
-    lista = [jogo1, jogo2, jogo3]
+@app.route('/')
+def ola():
     return render_template('lista.html', titulo = "Lista de Jogos", jogos = lista)
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo = 'Novo jogo')
 
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo = 'Lista de Jogos', jogos = lista)
+
 # não colocar essas configurações em produção
-app.run(host='127.0.0.1', port=8080)
+app.run(host='127.0.0.1', port=8080, debug=True)
